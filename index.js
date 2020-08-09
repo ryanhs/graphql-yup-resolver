1588e272d0cccf9938aed0c3a802797a83c0c0d5
ryanhs <mr.ryansilalahi@gmail.com>
2020-08-09 21:32:29 +0700

    with flaverr to remake the error

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const yup = require("yup"); // eslint-disable-line import/no-extraneous-dependencies
+const yup = require('yup'); // eslint-disable-line import/no-extraneous-dependencies
+const flaverr = require('flaverr'); // eslint-disable-line import/no-extraneous-dependencies
 
-module.exports = ({ args: validationArgs, resolver }) => async (
-  parent,
-  args = {},
-  context,
-  info
-) => {
-  try {
-    // validate args
-    let argsValidated = args;
-    const argsSchema = yup.object().shape(validationArgs);
-    argsValidated = await argsSchema.validate(args);
+module.exports = ({ args: validationArgs, resolver }) => async (parent, args = {}, context, info) => {
+  // validate args
+  let argsValidated = args;
+  const argsSchema = yup.object().shape(validationArgs);
+  argsValidated = await argsSchema.validate(args).catch((err) => {
+    throw flaverr({ code: 'E_INVALID_ARGINS' }, new Error(err.toString()));
+  });
 
-    return resolver(parent, argsValidated, context, info);
-  } catch (err) {
-    // make code for error, inspired from sails.js
-    err.code = "E_INVALID_ARGINS";
+  const result = await Promise.resolve(resolver(parent, argsValidated, context, info)).catch((err) => {
+    throw flaverr(
+      {
+        code: err.code || 'E_UNKNOWN_ERROR',
+      },
+      new Error(err.toString()),
+    );
+  });
 
-    throw err;
-  }
+  return result;
 };
